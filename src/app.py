49777e5036c97470c9606b28d85a529d1106606d
Anggie Alava <131820456+AngelesGit@users.noreply.github.com>
2023-08-23 16:36:39 +0000

    COMPLETADO

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -16,27 +16,59 @@ CORS(app)
 jackson_family = FamilyStructure("Jackson")
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
         "family": members
     }
-
-
     return jsonify(response_body), 200
+
+
+@app.route('/members/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    response_body = {
+        "family": member
+    }
+    return jsonify(response_body), 200
+
+
+@app.route('/members', methods=['POST'])
+def add_member():
+    data = request.get_json()
+    if not "age" in data:
+        return jsonify({"msg": "Missing parameter"}), 400
+    try:
+        result = jackson_family.add_member(data)
+        return jsonify({"msg": "Added successfully"}), 201
+    except Exception as err:
+        return jsonify({"msg": str(err)}), 400
+
+
+@app.route('/members/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    try:
+        result = jackson_family.delete_member(id)
+        return jsonify({"msg": "Deleted successfully"}), 200
+    except Exception as err:
+        return jsonify({"msg": str(err)}), 400
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
